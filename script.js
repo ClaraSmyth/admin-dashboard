b8512c64ba26acd8dfab0c55d8ad134b1473b9ee
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-11 16:28:17 +0100

    Cleaned up code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const width1000 = window.matchMedia('(min-width: 1000px)');
 
 width1000.addEventListener('change', () => {
     sbText.forEach(text => {
-        text.classList.toggle('display-none');
+        width1000.matches == true ? text.classList.remove('display-none') : text.classList.add('display-none');
     });
 });
 
