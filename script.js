4492fc129bb19eeaed3eebcec526d9fdd83bcf70
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-11 15:41:19 +0100

    Auto shrink sidebar at below 1000px

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const sbNavLink = document.querySelector('.sidebar-nav-link');
 const sbText = document.querySelectorAll('.sidebar-text');
+const width1000 = window.matchMedia('(min-width: 1000px)');
+
+width1000.addEventListener('change', () => {
+    sbText.forEach(text => {
+        text.classList.toggle('display-none');
+    });
+});
 
 sbNavLink.addEventListener('click', () => {
     sbText.forEach(text => {
