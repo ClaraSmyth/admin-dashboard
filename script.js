9a88608e66ae2f1bca2e7a36b8d4d9e061fffac9
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-11 17:25:32 +0100

    Made the page more responsive

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const sbNavLink = document.querySelector('.sidebar-nav-link');
 const sbText = document.querySelectorAll('.sidebar-text');
 const width1000 = window.matchMedia('(min-width: 1000px)');
+const mobileNavBtn = document.querySelector('.header-mobile-nav');
+const sidebar = document.querySelector('.sidebar');
 
+// Toggles the sidebar text on/off
+sbNavLink.addEventListener('click', () => {
+    sbText.forEach(text => {
+        text.classList.toggle('display-none');
+    });
+});
+
+// Turns the sidebar text off at smaller screen sizes by default
 width1000.addEventListener('change', () => {
     sbText.forEach(text => {
         width1000.matches == true ? text.classList.remove('display-none') : text.classList.add('display-none');
     });
 });
 
-sbNavLink.addEventListener('click', () => {
-    sbText.forEach(text => {
-        text.classList.toggle('display-none');
-    });
+// Toggles sidebar on mobile screen size
+mobileNavBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('display-none');
 });
